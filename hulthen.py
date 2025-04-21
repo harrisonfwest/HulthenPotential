5920cc85a5ac3deb053e19aecfcbc4959b86e76a
HwestCode <hwest@haverford.edu>
2025-04-21 19:32:10 -0400

    Update hulthen.py

--- hulthen.py
+++ hulthen.py
@@ -14,17 +14,16 @@ def hulthen_array(width = N_nodes, orbital = 1, delta = 0.025):
     A = np.diag(np.full(width, [1/(h**2)])) + np.diag(np.full(width-1, [-1/(2*(h**2))]), 1)\
     + np.diag(np.full(width-1, [-1/(2*(h**2))]), -1)
     for i in range(width):
-        A[i, i] += -orbital*(orbital + 1)/((i+1) * h)**2 + hulthen_discrete(i+1, delta)
+        A[i, i] += -orbital*(orbital + 1)/((i+1)**2 * h**2) + hulthen_discrete(i+1, delta)
     return A
 
 
-arr = hulthen_array()
+arr = hulthen_array(width = 25, orbital = 1, delta = 0.025)
 energy, wavefunction = eig(arr)
 
-# Plot the first excited state radial wave-function
-data = (wavefunction[1])[1::]
+data = (wavefunction[3])
 for i in range(len(data)):
     data[i] /= ((i+1) * h)
-data = data**2
+data = (data**2)[1::]
 plt.plot(data)
 plt.show()
