0f8b69ed2d31b5da5e837f2307266aae426dc376
HwestCode <hwest@haverford.edu>
2025-04-22 17:26:31 -0400

    Update hulthen.py

--- hulthen.py
+++ hulthen.py
@@ -2,30 +2,29 @@ import numpy as np
 from numpy.linalg import eig
 import matplotlib.pyplot as plt
 
-N_nodes = 15
-max_radius = 40
-h = int(max_radius / N_nodes)
-
+N_nodes = 50
+max_radius = 60
+h = max_radius / N_nodes
 
 def hulthen_discrete(r, delta):
     return delta * np.exp(-delta * r * h)/(1 - np.exp(-delta * r * h))
 
-def hulthen_array(width = N_nodes, orbital = 1, delta = 0.025):
+def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025):
+    h = size / width
     A = np.diag(np.full(width, [1/(h**2)])) + np.diag(np.full(width-1, [-1/(2*(h**2))]), 1)\
     + np.diag(np.full(width-1, [-1/(2*(h**2))]), -1)
     for i in range(width):
         A[i, i] += -orbital*(orbital + 1)/((i+1)**2 * h**2) + hulthen_discrete(i+1, delta)
+    A[0, 0] = 0
+    A[1, 0] = 0
+    A[-2, -1]
+    A[-1, -1] = 0
+    print(A)
     return A
 
 
-arr = hulthen_array(width = 60, orbital = 2, delta = 0.025)
+arr = hulthen_array(width = 20, size = 40, orbital = 1, delta = 0.025)
 energy, wavefunction = eig(arr)
-data = (wavefunction[3]**2)[3::]
+data = (wavefunction[2]**2)
 plt.plot(data)
-
-arr2 = hulthen_array(width = 60, orbital = 2, delta = 0.15)
-energy2, wavefunction2 = eig(arr2)
-data2 = (wavefunction[3]**2)[3::]
-plt.plot(data2)
-
 plt.show()
