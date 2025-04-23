85f9b1c0eb8cd0e9d5d29147cb3d34b13865a338
HwestCode <hwest@haverford.edu>
2025-04-23 11:52:21 -0400

    Update hulthen.py

--- hulthen.py
+++ hulthen.py
@@ -13,11 +13,13 @@ def hulthen_discrete(r, delta, orbital, currH):
     return t1 + (t2*(t3**2)*t4)
 
 def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025):
-    h = size / (width-2)
-    A = np.diag(np.full(width-2, [1/(h**2)])) + np.diag(np.full(width-3, [-1/(2*(h**2))]), 1)\
-    + np.diag(np.full(width-3, [-1/(2*(h**2))]), -1)
-    for i in range(width-2):
-        A[i, i] += -orbital*(orbital + 1)/((i+2) * h)**2 + hulthen_discrete(i+2, delta, orbital, h)
+    true_size = width - 2
+    h = size / width
+    A = np.diag(np.full(true_size, [1/(h**2)])) + np.diag(np.full(true_size-1, [-1/(2*(h**2))]), 1)\
+    + np.diag(np.full(true_size-1, [-1/(2*(h**2))]), -1)
+    for i in range(true_size):
+        true_r = i + 2
+        A[i, i] += -(orbital*(orbital + 1))/(true_r * h)**2 + hulthen_discrete(true_r, delta, orbital, h)
     return A
 
 # plt.plot(np.linspace(1, 200, 10000), \
@@ -25,7 +27,7 @@ def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025
 # plt.xscale('log')
 # plt.show()
 
-arr = hulthen_array(width = 30, size = 80, orbital = 1, delta = 0.025)
+arr = hulthen_array(width = 70, size = 200, orbital = 1, delta = 0.025)
 e, w = eig(arr)
 # for wave in w:
 #     wave[0] = 0
