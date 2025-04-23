6faf65c166f9b1afacc1cea56324a512b04a4487
HwestCode <hwest@haverford.edu>
2025-04-23 16:12:15 -0400

    Update hulthen.py

--- hulthen.py
+++ hulthen.py
@@ -15,24 +15,31 @@ def hulthen_discrete(r, delta, orbital, currH):
 def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025):
     true_size = width - 2
     h = size / width
-    A = np.diag(np.full(true_size, [1/(h**2)])) + np.diag(np.full(true_size-1, [-1/(2*(h**2))]), 1)\
-    + np.diag(np.full(true_size-1, [-1/(2*(h**2))]), -1)
+    A = np.diag(np.full(true_size, [1/(h**2)])) \
+      + np.diag(np.full(true_size-1, [-1/(2*(h**2))]), 1) \
+      + np.diag(np.full(true_size-1, [-1/(2*(h**2))]), -1)
     for i in range(true_size):
         true_r = i + 2
         A[i, i] += -(orbital*(orbital + 1))/(true_r * h)**2 + hulthen_discrete(true_r, delta, orbital, h)
     return A
 
+### Plot of Hulthen Potential:
 # plt.plot(np.linspace(1, 200, 10000), \
 #          hulthen_discrete(np.linspace(1, 200, 10000), 0.025, 1, currH = 1))
 # plt.xscale('log')
 # plt.show()
 
-arr = hulthen_array(width = 70, size = 200, orbital = 1, delta = 0.025)
+# Finding u(r) for 3p electron with screening parameter 0.025
+arr = hulthen_array(width = 80, size = 80, orbital = 1, delta = 0.025)
 e, w = eig(arr)
+
 # for wave in w:
+#     plt.plot(wave**2, alpha = 1)
+#     plt.show()
 #     wave[0] = 0
+
 sorted_e, sorted_w = zip(*sorted(zip(e, w)))
-plt.plot(sorted_w[2]**2)
-# func = np.append(0, np.append(sorted_w[2], 0))
-# plt.plot(func**2)
+# plt.plot(sorted_w[1]**2)
+func = np.append(0, np.append(sorted_w[1], 0))
+plt.plot(func**2)
 plt.show()
