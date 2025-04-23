ac71ff8b5c677ab64002e18a76db0cab06c8b7f0
HwestCode <hwest@haverford.edu>
2025-04-23 11:42:11 -0400

    Update hulthen.py

--- hulthen.py
+++ hulthen.py
@@ -20,17 +20,17 @@ def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025
         A[i, i] += -orbital*(orbital + 1)/((i+2) * h)**2 + hulthen_discrete(i+2, delta, orbital, h)
     return A
 
-plt.plot(np.linspace(1, 200, 10000), \
-         hulthen_discrete(np.linspace(1, 200, 10000), 0.025, 1, currH = 1))
-plt.xscale('log')
-plt.show()
+# plt.plot(np.linspace(1, 200, 10000), \
+#          hulthen_discrete(np.linspace(1, 200, 10000), 0.025, 1, currH = 1))
+# plt.xscale('log')
+# plt.show()
 
-arr = hulthen_array(width = 20, size = 50, orbital = 1, delta = 0.025)
+arr = hulthen_array(width = 30, size = 80, orbital = 1, delta = 0.025)
 e, w = eig(arr)
-for wave in w:
-    wave[0] = 0
+# for wave in w:
+#     wave[0] = 0
 sorted_e, sorted_w = zip(*sorted(zip(e, w)))
 plt.plot(sorted_w[2]**2)
-func = np.append(0, np.append(sorted_w[2], 0))
+# func = np.append(0, np.append(sorted_w[2], 0))
 # plt.plot(func**2)
 plt.show()
