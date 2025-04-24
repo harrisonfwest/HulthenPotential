895868905963c4b5c2c94679298d3833ff3b6b50
HwestCode <hwest@haverford.edu>
2025-04-24 13:03:02 -0400

    Update hulthen.py

--- hulthen.py
+++ hulthen.py
@@ -21,7 +21,7 @@ def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025
       + np.diag(np.full(true_size-1, [-1/(2*(h**2))]), -1)
     for i in range(true_size):
         true_r = i + 2
-        A[i, i] += -(orbital*(orbital + 1))/(true_r * h)**2 + hulthen_discrete(true_r, delta, orbital, h)
+        A[i, i] += (orbital*(orbital + 1))/(true_r * h)**2 + hulthen_discrete(true_r, delta, orbital, h)
     return A
 
 ### Plot of Hulthen Potential: ###
@@ -30,8 +30,9 @@ def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025
 # plt.xscale('log')
 # plt.show()
 
+
 # Finding u(r) for 3p electron with screening parameter 0.025
-arr = hulthen_array(width = 30, size = 40, orbital = 1, delta = 0.025)
+arr = hulthen_array(width = 60, size = 200, orbital = 1, delta = 0.025)
 e, w = eig(arr)
 
 # for wave in w:
@@ -41,6 +42,6 @@ e, w = eig(arr)
 
 sorted_e, sorted_w = zip(*sorted(zip(e, w)))
 # plt.plot(sorted_w[1]**2)
-func = np.append(0, np.append(sorted_w[1], 0))
-plt.plot(np.arange(0, 40, 40/30), func**2) # arange args are 0, size, size/width
+func = np.append(0, np.append(sorted_w[3], 0))
+plt.plot(np.arange(0, 200, 200/60), func**2) # arange args are 0, size, size/width
 plt.show()
