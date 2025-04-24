8d0b65d7ea574163897772a050a24e4899c97a61
HwestCode <hwest@haverford.edu>
2025-04-24 18:20:10 -0400

    Update hulthen.py

--- hulthen.py
+++ hulthen.py
@@ -19,12 +19,11 @@ def hulthen_discrete(r, delta, orbital, currH):
 # plt.show()
 
 def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025):
-    true_size = width - 1
     h = size / width
-    offDiags = np.full(shape = true_size - 1, fill_value = -1/(2 * h**2)) # off-diagonal entries
+    offDiags = np.full(shape = width - 1, fill_value = -1/(2 * h**2)) # off-diagonal entries
     offDiagA = np.diag(offDiags, 1) + np.diag(offDiags, -1)
 
-    diags = np.zeros(true_size)
+    diags = np.zeros(width)
     for i in range(len(diags)):
         true_index = i + 1
         diags[i] = (1/(h**2)) + (orbital * (orbital + 1))/(2 * (true_index * h)**2) + hulthen_discrete(true_index, delta, orbital, h)
@@ -32,10 +31,36 @@ def hulthen_array(width = N_nodes, size = max_radius, orbital = 1, delta = 0.025
     A = offDiagA + diagA
     return A
 
-arr = hulthen_array(width = 1000, size = 100, orbital = 1, delta = 0.025)
+arr = hulthen_array(width = 999, size = 100, orbital = 1, delta = 0.025)
 e, w = eig(arr)
 ordered_e = np.argsort(e)
-for i in ordered_e[:6]:
-    plt.plot(w[:,i]**2)
-    plt.title('E = ' + str(e[i]) + ' (l = 1)')
-    plt.show()
+
+# for i in ordered_e[:6]:
+#     plt.plot(w[:,i]**2)
+#     plt.title('E = ' + str(e[i]) + ' (l = 1)' + ', spot ' + str(i))
+#     plt.show()
+
+def simpson_integral(wavefunction, h):
+    length = int(len(wavefunction))
+    half_length = int(length / 2)
+
+    t1 = (1/3) * h
+
+    t2 = wavefunction[0] + wavefunction[-1]
+
+    t3 = 0
+    for i in range(1, half_length):
+        t3 += wavefunction[2*i - 1] + 2
+    t3 *= 4
+
+    t4 = 0
+    for i in range(1, half_length - 1):
+        t4 += wavefunction[2 * i]
+    t4 *= 2
+
+    return t1 * (t2 + t3 + t4)
+
+func = w[:,866]**2
+plt.plot(func)
+plt.show()
+print(simpson_integral(func, 100/999))
